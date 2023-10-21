cf71cf4a0fcbc1dfbafee224f07d67bde2b91d70
Kamil <kamil.nader1@gmail.com>
2023-10-21 12:20:53 +0200

    code and design of the landing page

---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+
+export const Heading = () => {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+        Your Ideas, Documents, & Plans. Welcome to{" "}
+        <span className="underline">NoteCraft</span>
+      </h1>
+      <h3 className="text-base sm:text-xl- md:text-2xl font-medium">
+        NoteCraft is the connected workspace where better, faster work happens.
+      </h3>
+      <Button>
+        Enter NoteCraft
+        <ArrowRight className="h-4 w-4 ml-2" />
+      </Button>
+    </div>
+  );
+};
