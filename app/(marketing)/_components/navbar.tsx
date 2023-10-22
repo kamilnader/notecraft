e35cbcb09c4dbe76972f668bd230975399cf5661
Kamil <kamil.nader1@gmail.com>
2023-10-22 09:42:47 +0200

    added clerk as auth and convex for database

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,14 +1,18 @@
 "use client";
 
-import Link from "next/link";
-
+import { useConvexAuth } from "convex/react";
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 export const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
 
   return (
@@ -20,6 +24,27 @@ export const Navbar = () => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get NoteCraft free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter NoteCraft</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://ideal-cockatoo-67.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
