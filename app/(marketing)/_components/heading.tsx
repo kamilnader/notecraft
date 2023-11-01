08e450aa8b5bcac7501d0cd9bd0e222a7352402d
Kamil <kamil.nader1@gmail.com>
2023-11-01 19:52:50 +0100

    Fix the logo and landing page content

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -10,13 +10,13 @@ import Link from "next/link";
 export const Heading = () => {
   const { isAuthenticated, isLoading } = useConvexAuth();
   return (
-    <div className="max-w-3xl space-y-4">
+    <div className="max-w-5xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-        Your Ideas, Documents, & Plans. Welcome to{" "}
-        <span className="underline">NoteCraft</span>
+        Where Your Ideas, Documents, and Plans Come Together.
       </h1>
       <h3 className="text-base sm:text-xl- md:text-2xl font-medium">
-        NoteCraft is the connected workspace where better, faster work happens.
+        Welcome to <span className="underline">NoteCraft</span> platform where
+        work becomes better and faster.
       </h3>
       {isLoading && (
         <div className="w-full flex items-center justify-center">
